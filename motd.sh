93d081346c8496af3ac3c56ffe078de3682a03dd
ashrafsharif <61407352+ashrafsharif@users.noreply.github.com>
2020-02-25 20:15:56 +0800

    Increase the lag notify

--- motd.sh
+++ motd.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-## Generate important MySQL status into motd
-## Also works as as cron, with -cron (require https://github.com/fabianonline/telegram.sh)
+## Generate important MySQL replication status into motd.
+## Also works as a cron (via -cron flag) and send notification via telegram, require in PATH: https://github.com/fabianonline/telegram.sh
+## Example in crontab:
+## * * * * * /root/.motd.sh -cron >/dev/null
 
 CRON=0
 if [ ! -z $1 ]; then
@@ -8,7 +10,7 @@ if [ ! -z $1 ]; then
 fi
 HOSTNAME=$(hostname)
 UPTIME=$(uptime -p)
-MYSQL_COMMAND='mysql --connect-timeout=3 -A -Bse'
+MYSQL_COMMAND='mysql --connect-timeout=2 -A -Bse'
 MYSQL_READONLY=$(${MYSQL_COMMAND} 'SHOW GLOBAL VARIABLES LIKE "read_only"' | awk {'print $2'})
 TIER='Production'
 #PREFER_ROLE='Slave'
@@ -17,8 +19,9 @@ MAIN_IP=$(hostname -I)
 CHECK_MYSQL_REPLICATION=$(${MYSQL_COMMAND} 'SHOW SLAVE STATUS\G' | egrep 'Slave_.*_Running: Yes$')
 MYSQL_MASTER=$(${MYSQL_COMMAND} 'SHOW SLAVE STATUS\G' | grep Master_Host | awk {'print $2'})
 MYSQL_UPTIME=$(${MYSQL_COMMAND} 'SELECT TIME_FORMAT(SEC_TO_TIME(VARIABLE_VALUE ),"%Hh %im")  AS Uptime FROM information_schema.GLOBAL_STATUS WHERE VARIABLE_NAME="Uptime"')
-alert_dir=/usr/local/src
+ALERT_DIR=/usr/local/src
 TELEGRAM=$(which telegram)
+LAG_NOTIFY=5
 
 bold=$(tput bold)
 red=$(tput setaf 1)
@@ -29,8 +32,9 @@ function send_alert()
 {
 	local alert_id=$1
 	local notification=$*
-	local alert_path=$alert_dir/$alert_id
+	local alert_path=$ALERT_DIR/$alert_id
 
+	# only trigger alert once. alert_id 000 means clear the alarm
 	if [ $alert_id -eq 000 ]; then
 		$TELEGRAM "Alarm cleared"
 	else
@@ -49,13 +53,15 @@ if [ $MYSQL_READONLY == 'ON' ]; then
                 if [ $lag -eq 0 ]; then
                         REPLICATION_STATUS="${green}Healthy  "
 			if [ $CRON -eq 1 ]; then
-				[ -e $alert_dir/100 ] && rm -Rf $alert_dir/100 && send_alert 000
-				[ -e $alert_dir/101 ] && rm -Rf $alert_dir/101 && send_alert 000
-				[ -e $alert_dir/102 ] && rm -Rf $alert_dir/102 && send_alert 000
+				[ -e $ALERT_DIR/100 ] && rm -Rf $ALERT_DIR/100 && send_alert 000
+				[ -e $ALERT_DIR/101 ] && rm -Rf $ALERT_DIR/101 && send_alert 000
+				[ -e $ALERT_DIR/102 ] && rm -Rf $ALERT_DIR/102 && send_alert 000
 			fi
                 else
                         REPLICATION_STATUS="${red}Lagging ${lag}s"
-			[ $CRON -eq 1 ] && send_alert 101 "[$HOSTNAME] Replication is lagging: $lag s."
+			if [ $CRON -eq 1 ]; then
+				[ $lag -gt $LAG_NOTIFY ] && send_alert 101 "[$HOSTNAME] Replication is lagging: $lag s."
+			fi
                 fi
         else
                 REPLICATION_STATUS=${red}Unhealthy
@@ -66,7 +72,7 @@ elif [ $MYSQL_READONLY == 'OFF' ]; then
         CURRENT_MYSQL_ROLE='Master'
         SLAVE_HOSTS=$(${MYSQL_COMMAND} 'SHOW SLAVE HOSTS' | awk {'print $2'})
 	if [ $CRON -eq 1 ]; then
-		[ -e $alert_dir/100 ] && rm -Rf $alert_dir/100 && send_alert 000
+		[ -e $ALERT_DIR/100 ] && rm -Rf $ALERT_DIR/100 && send_alert 000
 	fi
 else
         MYSQL_SHOW=0
