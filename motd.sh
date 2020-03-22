fd9b9b13aed9e5eb27d4e769c222530ad3f4fbf4
ashrafsharif <acopkirk@gmail.com>
2020-03-22 02:27:33 +0000

    added replbroken, slave lag increased to 5

--- motd.sh
+++ motd.sh
@@ -13,8 +13,8 @@ UPTIME=$(uptime -p)
 MYSQL_COMMAND='mysql --connect-timeout=2 -A -Bse'
 MYSQL_READONLY=$(${MYSQL_COMMAND} 'SHOW GLOBAL VARIABLES LIKE "read_only"' | awk {'print $2'})
 TIER='Production'
-#PREFER_ROLE='Slave'
-PREFER_ROLE='Master'
+PREFER_ROLE='Slave'
+#PREFER_ROLE='Master'
 MAIN_IP=$(hostname -I)
 CHECK_MYSQL_REPLICATION=$(${MYSQL_COMMAND} 'SHOW SLAVE STATUS\G' | egrep 'Slave_.*_Running: Yes$')
 MYSQL_MASTER=$(${MYSQL_COMMAND} 'SHOW SLAVE STATUS\G' | grep Master_Host | awk {'print $2'})
@@ -58,9 +58,14 @@ if [ $MYSQL_READONLY == 'ON' ]; then
 				[ -e $ALERT_DIR/102 ] && rm -Rf $ALERT_DIR/102 && send_alert 000
 			fi
                 else
-                        REPLICATION_STATUS="${red}Lagging ${lag}s"
-			if [ $CRON -eq 1 ]; then
-				[ $lag -gt $LAG_NOTIFY ] && send_alert 101 "[$HOSTNAME] Replication is lagging: $lag s."
+			if [ $lag == 'NULL' ]; then
+				REPLICATION_STATUS=${red}ReplBroken
+				[ $CRON -eq 1 ] && send_alert 103 "[$HOSTNAME] Replication is broken: $lag s."
+			else
+	                        REPLICATION_STATUS="${red}Lagging ${lag}s"
+				if [ $CRON -eq 1 ]; then
+					[ $lag -gt $LAG_NOTIFY ] && send_alert 101 "[$HOSTNAME] Replication is lagging: $lag s."
+				fi
 			fi
                 fi
         else
